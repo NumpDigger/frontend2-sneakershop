ae9388d5f023ba890c1090e3448ddc301e9f0856
NumpDigger <emiyatkul@gmail.com>
2026-03-17 11:15:19 +0500

    Big update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,19 @@ const products = [
         id: 1,
         title: "Nike Blazzer Mid Suede",
         price: 79.99,
-        imgUrl: "../assets/product/sneaker-1.jpg"
+        imgUrl: "assets/product/sneaker-1.jpg"
     },
     {
         id: 2,
         title: "Nike Air Max 207",
         price: 59.99,
-        imgUrl: "../assets/products/sneaker-2.jpg"
+        imgUrl: "assets/products/sneaker-2.jpg"
     },
     {
         id: 3,
         title: "Nike Blazer Mid Suede",
         price: 39.99,
-        imgUrl: "../assets/products/sneaker-3.jpg"      
+        imgUrl: "assets/products/sneaker-3.jpg"      
     },
 ]
 
